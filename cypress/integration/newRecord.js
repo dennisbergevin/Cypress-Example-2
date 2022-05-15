e33aa85bb3ee0801f7f67015bc06fe6fb5e4089a
Dennis Bergevin <dennisbergevin@dennis-macbookair.lan>
2022-05-15 10:49:05 -0400

    abstracted api data assertions

--- cypress/integration/newRecord.js
+++ cypress/integration/newRecord.js
@@ -19,12 +19,12 @@ describe("User can successfully create and delete new record", () => {
     });
     record.recordSavedMessage.should("be.visible");
     cy.wait("@recordPost").then((interception) => {
-      expect(interception.response.body.values.aFjm80LnbJf780V6p).to.eq(
-        "Test City"
-      );
-      expect(interception.response.body.values.aHdR_gHQmRT8ItVTL).to.eq("Test");
-      expect(interception.response.body.values.aHxOeHmCTIGd_hg1b).to.eq("User");
-      expect(interception.response.statusCode).to.equal(200);
+      const field = interception.response.body.values;
+
+      expect(interception.response.statusCode).to.eq(200);
+      expect(field.aFjm80LnbJf780V6p).to.eq("Test City");
+      expect(field.aHdR_gHQmRT8ItVTL).to.eq("Test");
+      expect(field.aHxOeHmCTIGd_hg1b).to.eq("User");
     });
   });
 
@@ -36,41 +36,23 @@ describe("User can successfully create and delete new record", () => {
     });
     record.recordSavedMessage.should("be.visible");
     cy.wait("@recordGet").should((interception) => {
-      expect(interception.response.statusCode).to.equal(200);
-      expect(interception.response.body.values.aFjm80LnbJf780V6p).to.eq(
-        "Test City"
-      );
-      expect(interception.response.body.values.aHdR_gHQmRT8ItVTL).to.eq("Test");
-      expect(interception.response.body.values.aHxOeHmCTIGd_hg1b).to.eq("User");
-      expect(interception.response.body.values.aJDBDjjIFiTemxLGc).to.eq(
-        "123 Main Street"
-      );
-      expect(interception.response.body.values.aGgc3qp6gt3dDR_na).to.eq(
-        "test@test.com"
-      );
-      expect(interception.response.body.values.aG_YiSItNjs7vGALq).to.eq(
-        "12345"
-      );
-      expect(interception.response.body.values.aJX7sLD3xZH9TlVps).to.eq(
-        "(999) 999-9999"
-      );
-      expect(interception.response.body.values.aJr4VxhqeQ4fAZgO7).to.eq(
-        "Testing Something"
-      );
-      expect(interception.response.body.values.aIaHwVkkr_seOK096).to.eq("TX");
-      expect(interception.response.body.values.aKTyoAgO27gfZC0Vd).to.eq(12345);
-      expect(interception.response.body.values.aFPpUOs0uSrcRCKYZ.value).to.eq(
-        "Dental"
-      );
-      expect(interception.response.body.values.aGMfQEKK_1G7WdqEK.value).to.eq(
-        "Engineering"
-      );
-      expect(interception.response.body.values.aHJVM3nf4afdc4Kv5.value).to.eq(
-        "The Beatles"
-      );
-      expect(interception.response.body.values.aIuEa7EWYrg958AiM.value).to.eq(
-        "Full Time"
-      );
+      const field = interception.response.body.values;
+
+      expect(interception.response.statusCode).to.eq(200);
+      expect(field.aFjm80LnbJf780V6p).to.eq("Test City");
+      expect(field.aHdR_gHQmRT8ItVTL).to.eq("Test");
+      expect(field.aHxOeHmCTIGd_hg1b).to.eq("User");
+      expect(field.aJDBDjjIFiTemxLGc).to.eq("123 Main Street");
+      expect(field.aGgc3qp6gt3dDR_na).to.eq("test@test.com");
+      expect(field.aG_YiSItNjs7vGALq).to.eq("12345");
+      expect(field.aJX7sLD3xZH9TlVps).to.eq("(999) 999-9999");
+      expect(field.aJr4VxhqeQ4fAZgO7).to.eq("Testing Something");
+      expect(field.aIaHwVkkr_seOK096).to.eq("TX");
+      expect(field.aKTyoAgO27gfZC0Vd).to.eq(12345);
+      expect(field.aFPpUOs0uSrcRCKYZ.value).to.eq("Dental");
+      expect(field.aGMfQEKK_1G7WdqEK.value).to.eq("Engineering");
+      expect(field.aHJVM3nf4afdc4Kv5.value).to.eq("The Beatles");
+      expect(field.aIuEa7EWYrg958AiM.value).to.eq("Full Time");
     });
   });
 
@@ -88,7 +70,7 @@ describe("User can successfully create and delete new record", () => {
       record.confirmDeleteOkBtn.should("be.visible").click();
     });
     cy.wait("@recordDelete").should((interception) => {
-      expect(interception.response.statusCode).to.equal(204);
+      expect(interception.response.statusCode).to.eq(204);
     });
   });
 
